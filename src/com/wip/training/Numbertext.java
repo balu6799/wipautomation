6834f5f61c68f75f7662afc3d49a8aeb6be7e587
venkata subrahmanyam kancharla <venkata subrahmanyam kancharla>
2017-02-11 11:35:45 -0800

    NumberText.java

---
+++ src/com/wip/training/Numbertext.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.wip.training;
+
+/**
+ * @author balu6
+ *
+ */
+public class Numbertext {
+    public static void main(String[] args) {
+        String [] digits = new String [] {
+                "Zero","One","Two","Three"
+        };
+        int num = 123456;
+        while (num > 0) {
+            int digit = num % 10;
+            System.out.println(num % 10);
+            num =num/10;
+        }
+
+    }
+}
